60e31417bc4508abe0bbed5d8e7e9c954cdc3c0e
ant1pink <ruili2@publicisgroupe.net>
2018-03-05 18:28:20 +0000

    logood

---
+++ keras_frcnn/simple_parser_logood.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'R.Li'
+'''
+# Copyright (C) 2017 Publicis Groupe Media UK
+# Data Sciences UK
+#
+# This file is generated for an internal project within ZO and
+# should not be copied and/or distributed in part or as whole.
+#
+# In case of any queries, please contact
+#        AttributionModelling@zenithoptimedia.co.uk;
+#        ray.li@publicismedia.com
+#
+'''
